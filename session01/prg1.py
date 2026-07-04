d60afcabe0c69325e154b4d9b4bb434d7f6d6145
Abbas baghestany <299552785+baghestany@users.noreply.github.com>
2026-07-04 22:46:11 +0330

    prg1.py completed

--- session01/prg1.py
+++ session01/prg1.py
@@ -1,6 +1,31 @@
-def areas(x,y):
-    return x*y
+def calculate_status(totalamount):
+    if totalamount >= 1000000 :
+        return "High Volume"
+    if totalamount < 500000 :
+        return "Low Volume"
+    else :
+        return "Medium Volume"
 
-x = float(input("Enter price per meter : "))
-y = float(input("Enter House meters : "))
-print (f"Total Price is: " , areas(x,y))
+#print(calculate_status(700000))
+try : 
+ Tr_count= int(input("How many transactions do you want to enter? "))
+ if(Tr_count>0) :
+  totalamount =0
+  for i in range(1,Tr_count+1):
+       while True :
+        try :
+         amount = int(input(f"Enter amount for transaction {i}: "))
+         totalamount = totalamount+amount
+         break
+        except ValueError :
+          print("Invalid amount. Try again")
+ 
+  averageamount = totalamount/Tr_count
+  print(f"Transaction Count : {Tr_count}")
+  print(f"Total amount : {totalamount}")
+  print(f"Average Amount : {averageamount}")
+  print(calculate_status(totalamount))
+ else :
+    print("You should have at least one Transaction")
+except ValueError :
+   print("Invalid Number, try again")
